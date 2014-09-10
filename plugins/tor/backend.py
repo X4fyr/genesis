7963abb3815da31b575cdfafe4e551d0108d1270
X4fyr <benedikt_v@web.de>
2014-09-10 04:02:38 +0200

    Made gui and first status fetch
    - Created layout with all dialogues
      - all made in layout/main.xml, so theres no generating in main.py
        planned
      - planned to set values instead of complete generation
      - only hidden services will be generated
    - added Get method in backend
      - currently just for status
      - expanded in future for dialogue values
      - output as dict, maybe filling values with for loop in future

--- plugins/tor/backend.py
+++ plugins/tor/backend.py
@@ -11,94 +11,100 @@ Description: backend oparations
 from stem.control import Controller
 
 
-class Status(object):
+class Get(object):
     """Collecting and formatting status-data of tor"""
-    GETINFO = {'config-file': 'config-file',
-               'address-mapping': 'address-mappings/all',
-               'ext-ip': 'address',
-               'circ-stat': 'circuit-status',
-               'read': 'traffic/read',
-               'written': 'traffic/written',
-               'uid': 'process/uid',
-               'circ_est': 'status/circuit-established',
-               'enough': 'status/enough-dir-info',
-               'good': 'status/good-server-descriptor',
-               'accepted': 'status/accepted-server-descriptor'}
-    controller = Controller.from_port(port=9051)    # TODO: fetch from settings
 
-    def __init__(self):
+    try:
+        _controller = Controller.from_port(port=9051)
+    except:
+        pass
+
+    def __init__(self, debug):  # TODO: figure out what debug is and where it come from
         """initiate connection"""
-        self.controller.authenticate()  # TODO: fix permissions in torrc
-
-    def __del__(self):
-        """close connection to socket"""
-        self.controller.close()
-
-    def print_status(self):
-        """print status (for debbuging)"""
-        print("Version: %s" % self.controller.get_version())
-        print("Process: pid:%s user:%s" % (self.controller.get_pid(),
-                                           self.controller.get_user()))
-        print("Read: %s bytes Written: %s bytes" % (
-            self.controller.get_info(self.GETINFO['read']),
-            self.controller.get_info(self.GETINFO['written'])))
-        print("Current exit policy: %s" %
-              self.controller.get_exit_policy().summary())
-#        print("External IP: %s" %
-#              self.controller.get_info(self.GETINFO['ext-ip']))
-#        print("Circuit Status: %s" %
-#              self.controller.get_info(self.GETINFO['circ-stat']))
-        if self.controller.get_info(self.GETINFO['circ_est']):
-            print("Circuit established!")
-        elif self.controller.get_info(self.GETINFO['accepted']):
-            print("Descriptor accepted")
-        elif self.controller.get_info(self.GETINFO['good']):
-            print("Good Descriptor")
-        elif self.controller.get_info(self.GETINFO['enough']):
-            print("Enough Dir Information")
+        try:
+            self._controller.authenticate()     # TODO: Needs pre config
+        except Exception, authentication:
+            raise authentication
         else:
-            print("Something is going wrong.")
-
-
-class Configuration(object):
-    """Get and set Configuration"""
-    controller = Controller.from_port(port=9051)
-
-    def __init__(self):
-        """Initialte socket connection """
-        self.controller.authenticate()
+            self.version = self._controller.get_version()
 
     def __del__(self):
         """close connection to socket"""
-        self.controller.close()
+        try:
+            self._controller.close()
+        except:
+            pass
 
-    def hidden_service(self):
-        """get and format config
-        :returns: dict with HiddenService keys
-        """
-        conf = self.controller.get_conf_map('HiddenServiceOptions')
-        # TODO: waiting for stem patch
-        #       https://trac.torproject.org/projects/tor/ticket/12533
-        #       Support only one Hidden Service till patch
-        return conf
-
-    def general(self):
-        """get general config
-        :returns: TODO
-        """
-        KEYS = ['SocksPort',
-                'DataDirectory',
-                'ControlPort',
-                'ControlListenAddress',
-                'ControlSocket',
-                'CookieAuthFileGroupReadable',
-                'HTTPProxy',
-                'HTTPProxyAuthenticator',
-                'HTTPSProxy',
-                'HTTPSProxyAuthenticator',
-                'Socks4Proxy',
-                'Socks5Proxy',
-                'Socks5ProxyPassword',
-                'SOCKSListenAddress']
-        conf = self.controller.get_conf_map(KEYS)
-        return conf
+    def status(self):
+        """print status (for debbuging)"""
+        _GETINFO = {
+            'config-file': 'config-file',
+            'address-mapping': 'address-mappings/all',
+            'ext_ip': 'address',
+            'read': 'traffic/read',
+            'written': 'traffic/written',
+            'circ_est': 'status/circuit-established',
+            'enough': 'status/enough-dir-info',
+            'good': 'status/good-server-descriptor',
+            'accepted': 'status/accepted-server-descriptor'
+            }
+        if self._controller.get_info(_GETINFO["circ_est"]):
+            _circ_est = "Running"
+        else:
+            _circ_est = "Not Running. Check logs."
+        try:
+            _ext_ip = self._controller.get_info(_GETINFO["ext_ip"])
+        except:
+            _ext_ip = "Unable to fetch IP."
+        _status_package = {
+            'version': self._controller.get_version(),
+            'status': _circ_est,
+            'ext_ip': _ext_ip,
+            'read': self._controller.get_info(_GETINFO["read"]) + " B",
+            'written': self._controller.get_info(_GETINFO["written"]) + " B"
+            }
+        return _status_package
+# 
+# 
+# class Configuration(object):
+#     """Get and set Configuration"""
+#     controller = Controller.from_port(port=9051)    # TODO: change to _controller
+# 
+#     def __init__(self):
+#         """Initialte socket connection """
+#         self.controller.authenticate()
+# 
+#     def __del__(self):
+#         """close connection to socket"""
+#         self.controller.close()
+# 
+#     def hidden_service(self):
+#         """get and format config
+#         :returns: dict with HiddenService keys
+#         """
+#         conf = self.controller.get_conf_map('HiddenServiceOptions')
+#         # TODO: waiting for stem patch
+#         #       https://trac.torproject.org/projects/tor/ticket/12533
+#         #       Support only one Hidden Service till patch
+#         return conf
+# 
+#     def general(self):
+#         """get general config
+#         :returns: TODO
+#         """
+#         KEYS = ['SocksPort',
+#                 'DataDirectory',
+#                 'ControlPort',
+#                 'ControlListenAddress',
+#                 'ControlSocket',
+#                 'CookieAuthFileGroupReadable',
+#                 'HTTPProxy',
+#                 'HTTPProxyAuthenticator',
+#                 'HTTPSProxy',
+#                 'HTTPSProxyAuthenticator',
+#                 'Socks4Proxy',
+#                 'Socks5Proxy',
+#                 'Socks5ProxyPassword',
+#                 'SOCKSListenAddress']
+#         conf = self.controller.get_conf_map(KEYS)
+#         return conf
--- plugins/tor/main.py
+++ plugins/tor/main.py
@@ -10,6 +10,7 @@ Description: first draft
 
 from genesis.ui import *
 from genesis.api import *
+from backend import Get
 
 
 class Tor(CategoryPlugin):
@@ -29,4 +30,53 @@ class Tor(CategoryPlugin):
 
     def get_ui(self):
         """create ui"""
-        pass
+        ui = self.app.inflate('tor:main')
+        try:
+            get = Get(self)
+        except:
+            ui.remove("main")
+            ui.remove("dlgClientConf")
+            ui.remove("dlgRelayConf")
+            ui.remove("dlgProxyFw")
+        else:
+            ui.remove("fault")
+            status = get.status()
+            ui.find("version").set("text", status["version"])
+            ui.find("status").set("text", status["status"])
+            ui.find("ext_ip").set("text", status["ext_ip"])
+            ui.find("read").set("text", status["read"])
+            ui.find("written").set("text", status["written"])
+
+            # Dialog control
+            if self._clientconfig is None:
+                ui.remove("dlgClientConf")
+
+            if self._relayconfig is None:
+                ui.remove("dlgRelayConf")
+
+            if self._proxyfw is None:
+                ui.remove("dlgProxyFw")
+            del get
+        finally:
+            return ui
+
+    @event('button/click')
+    def on_click(self, event, params, vars=None):
+        """Allocate buttons"""
+        if params[0] == 'btnClientConf':
+            self._clientconfig = 1
+        elif params[0] == 'btnRelayConf':
+            self._relayconfig = 1
+        elif params[0] == 'btnProxyFw':
+            self._proxyfw = 1
+
+    @event('dialog/submit')
+    @event('form/submit')
+    def on_submit(self, event, params, vars=None):
+        """Handle form submits"""
+        if params[0] == 'dlgClientConf':
+            self._clientconfig = None
+        elif params[0] == 'dlgRelayConf':
+            self._relayconfig = None
+        elif params[0] == 'dlgProxyFw':
+            self._proxyfw = None
